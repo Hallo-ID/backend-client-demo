961de8cf0138540a8ea360953e340fbe181a6796
Diego Rojas <darguelles.rojas91@gmail.com>
2022-11-09 01:06:20 +0100

    Updated client

--- src/main/java/com/halloid/example/backendclientdemo/controller/AuthenticationController.java
+++ src/main/java/com/halloid/example/backendclientdemo/controller/AuthenticationController.java
@@ -4,28 +4,41 @@ import com.halloid.example.backendclientdemo.service.TokenService;
 import com.halloid.sdk.model.AuthenticationResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth/token")
+@CrossOrigin(origins = "http://localhost:8090")
 public class AuthenticationController {
 
     @Autowired
     private TokenService tokenService;
 
     @GetMapping
-    public ResponseEntity<String> generateToken(){
+    public ResponseEntity<TokenWrapper> generateToken(){
         String token = tokenService.generateToken();
-        return ResponseEntity.ok(token);
+        return ResponseEntity.ok(new TokenWrapper(token));
     }
 
     @GetMapping("{token}/verify")
     public ResponseEntity<AuthenticationResponse> verifyToken(@PathVariable String token) {
         AuthenticationResponse authPayload = tokenService.validateToken(token);
         return ResponseEntity.ok(authPayload);
+    }
+
+    private class TokenWrapper {
+        private String token;
+        public TokenWrapper(String token) {
+            this.token = token;
+        }
+
+        public String getToken() {
+            return token;
+        }
+
+        public void setToken(String token) {
+            this.token = token;
+        }
     }
 
 }
