1dc8d3e9b833e9c303353c4d20d2706899f04d35
Diego Rojas <darguelles.rojas91@gmail.com>
2022-12-01 15:34:56 +0100

    TEST DEMO WAES

--- src/main/java/com/halloid/example/backendclientdemo/controller/AuthenticationController.java
+++ src/main/java/com/halloid/example/backendclientdemo/controller/AuthenticationController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("auth/token")
-@CrossOrigin(origins = "http://localhost:8090")
+@CrossOrigin(origins = "http://localhost:3001")
 public class AuthenticationController {
 
     @Autowired
